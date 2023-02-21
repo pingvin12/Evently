97ef632a2b4ad900cab741a39a1ec81ec7e27f69
pingvin12 <fenyesj@gmail.com>
2023-02-21 02:00:13 +0100

    frontend revamp

---
+++ ClientBackend/ClientBackend/Controllers/AuthenticationController.cs
@@ -0,0 +1,93 @@
+﻿using ClientBackend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web.Resource;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ClientBackend.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    [ApiController]
+    [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+    public class AuthenticationController
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Register([FromBody] AppUser registerUser)
+        {
+            var exists = _userManager.FindByEmailAsync(registerUser.Email);
+            if (exists != null)
+            {
+                return new NotFoundResult();
+            }
+            var res = _userManager.CreateAsync(registerUser);
+            if (res.IsCompletedSuccessfully)
+            {
+                return new OkResult();
+            }
+            else
+            {
+                return new BadRequestResult();
+            }
+        }
+
+        private JwtSecurityToken GetToken(List<Claim> authClaims)
+        {
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddHours(2),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+
+            return token;
+        }
+
+
+        [HttpGet("{email}/{password}")]
+        public IActionResult Login(string email, string password)
+        {
+            var res = _userManager.FindByEmailAsync(email).Result;
+            if (res != null && _userManager.CheckPasswordAsync(res, password).Result)
+            {
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, res.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+                var userRoles = _userManager.GetRolesAsync(res).Result;
+                foreach (var role in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, role));
+                }
+
+                var jwtToken = GetToken(authClaims);
+                var response = new { token = new JwtSecurityTokenHandler().WriteToken(jwtToken), expiration = jwtToken.ValidTo };
+                return new OkObjectResult(response);
+
+                }
+            return new UnauthorizedResult();
+        }
+    }
+}
---
+++ ClientBackend/ClientBackend/Migrations/20230219180943_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClientBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "events");
+
+            migrationBuilder.CreateTable(
+                name: "events",
+                schema: "events",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false),
+                    name = table.Column<string>(type: "character varying", nullable: true),
+                    location = table.Column<string>(type: "character varying", nullable: true),
+                    country = table.Column<string>(type: "character varying", nullable: true),
+                    capacity = table.Column<decimal[]>(type: "numeric[]", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("events_pk", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "events_id_idx",
+                schema: "events",
+                table: "events",
+                column: "id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "events",
+                schema: "events");
+        }
+    }
+}
